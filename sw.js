5763b3a9d99746e637d36ed11489764626295c2f
b70040-sudo <b70040@gmail.com>
2026-08-18 17:39:10 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const C='jw-kenting-travel-style-v1';const A=['./','./index.html','./manifest.webmanifest','./icon.svg','./kentimg.jpg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
